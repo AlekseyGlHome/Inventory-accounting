55b3e56543ad96b51fc8c1a4ff6c1d9485c7c497
alekseygl1978 <44414087+alekseygl1978@users.noreply.github.com>
2021-12-29 11:51:00 +0300

    Создание проекта

---
+++ src/main/java/ru/home/inventoryaccounting/InventoryAccountingApplication.java
@@ -0,0 +1,13 @@
+package ru.home.inventoryaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/home/inventoryaccounting/InventoryAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.inventoryaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
