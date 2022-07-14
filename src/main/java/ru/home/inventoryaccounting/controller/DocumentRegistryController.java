29a01289aa2899034823e4a989cbdef57040f4a0
AlekseyGlHome <Glushenko78@yandex.ru>
2022-07-14 16:55:11 +0300

    Создание структуры для регистрации документов

---
+++ src/main/java/ru/home/inventoryaccounting/controller/DocumentRegistryController.java
@@ -0,0 +1,22 @@
+package ru.home.inventoryaccounting.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/registration")
+public class DocumentRegistryController {
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Boolean> registry(@PathVariable Long id){
+
+
+        return ResponseEntity.ok(true);
+    }
+
+}
---
+++ src/main/java/ru/home/inventoryaccounting/domain/entity/DocumentRegistration.java
@@ -0,0 +1,26 @@
+package ru.home.inventoryaccounting.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "document_registration")
+public class DocumentRegistration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    //@Column(name = "document_header_id", nullable = false)
+    @OneToOne()
+    @JoinColumn(name = "document_header_id")
+    private DocumentHeaderEntity documentHeaderEntity;
+
+}
