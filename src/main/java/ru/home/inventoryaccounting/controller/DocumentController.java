3d9c6134c9325bd9ed0827f02ba6f028d1a00fd2
AlekseyGlHome <Glushenko78@yandex.ru>
2022-10-19 14:23:50 +0300

    Изменение форматирования java кода

--- src/main/java/ru/home/inventoryaccounting/controller/DocumentController.java
+++ src/main/java/ru/home/inventoryaccounting/controller/DocumentController.java
@@ -18,55 +18,67 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/v1/documents")
 public class DocumentController {
-    private final DocumentService documentsHeaderService;
+  private final DocumentService documentsHeaderService;
 
-    @GetMapping()
-    public ResponseEntity<DtoResponse<DocumentHeaderDto>> getByAllOrFilter(
-            @RequestParam(name = "offset", defaultValue = "0") int offset,
-            @RequestParam(name = "limit", defaultValue = "10") int limit,
-            @RequestParam(name = "query", defaultValue = "") String query,
-            @RequestParam(name = "intervalStart", defaultValue = "") LocalDate intervalStart,
-            @RequestParam(name = "intervalEnd", defaultValue = "") LocalDate intervalEnd,
-            @RequestParam(name = "partnerId", defaultValue = "0") Long partnerId,
-            @RequestParam(name = "warehouseId", defaultValue = "0") Long warehouseId,
-            @RequestParam(name = "typeDok", defaultValue = "0") Integer typeDok,
-            @RequestParam(name = "sortColumns", defaultValue = "date") String[] sortColumns,
-            @RequestParam(name = "sortingDirection", defaultValue = "ASC") String sortingDirection) {
-        RequestParametersForDocHeader parameter =
-                RequestParameterUtil.getObjectOfRequestParametersOfDocumentHeader(offset, limit, query, intervalStart,
-                        intervalEnd, partnerId, warehouseId, typeDok, sortColumns, sortingDirection);
-        return ResponseEntity.ok(documentsHeaderService.selectQuery(parameter));
-    }
+  @GetMapping()
+  public ResponseEntity<DtoResponse<DocumentHeaderDto>> getByAllOrFilter(
+      @RequestParam(name = "offset", defaultValue = "0") int offset,
+      @RequestParam(name = "limit", defaultValue = "10") int limit,
+      @RequestParam(name = "query", defaultValue = "") String query,
+      @RequestParam(name = "intervalStart", defaultValue = "") LocalDate intervalStart,
+      @RequestParam(name = "intervalEnd", defaultValue = "") LocalDate intervalEnd,
+      @RequestParam(name = "partnerId", defaultValue = "0") Long partnerId,
+      @RequestParam(name = "warehouseId", defaultValue = "0") Long warehouseId,
+      @RequestParam(name = "typeDok", defaultValue = "0") Integer typeDok,
+      @RequestParam(name = "sortColumns", defaultValue = "date") String[] sortColumns,
+      @RequestParam(name = "sortingDirection", defaultValue = "ASC") String sortingDirection) {
+    RequestParametersForDocHeader parameter =
+        RequestParameterUtil.getObjectOfRequestParametersOfDocumentHeader(
+            offset,
+            limit,
+            query,
+            intervalStart,
+            intervalEnd,
+            partnerId,
+            warehouseId,
+            typeDok,
+            sortColumns,
+            sortingDirection);
+    return ResponseEntity.ok(documentsHeaderService.selectQuery(parameter));
+  }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<DtoResponse<DocumentHeaderAndBodyDto>> getById(@PathVariable long id) {
-        return ResponseEntity.ok(new DtoResponse<>(1L, List.of(documentsHeaderService.getFullById(id))));
-    }
+  @GetMapping("/{id}")
+  public ResponseEntity<DtoResponse<DocumentHeaderAndBodyDto>> getById(@PathVariable long id) {
+    return ResponseEntity.ok(
+        new DtoResponse<>(1L, List.of(documentsHeaderService.getFullById(id))));
+  }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<DtoResponse<DocumentHeaderAndBodyDto>> update(@PathVariable long id,
-                                                                        @RequestBody DocumentHeaderRequest request) {
-        return ResponseEntity.ok(new DtoResponse<>(1L, List.of(documentsHeaderService.update(id, request))));
-    }
+  @PutMapping("/{id}")
+  public ResponseEntity<DtoResponse<DocumentHeaderAndBodyDto>> update(
+      @PathVariable long id, @RequestBody DocumentHeaderRequest request) {
+    return ResponseEntity.ok(
+        new DtoResponse<>(1L, List.of(documentsHeaderService.update(id, request))));
+  }
 
-    @PostMapping()
-    public ResponseEntity<DtoResponse<DocumentHeaderAndBodyDto>> add(@RequestBody DocumentHeaderRequest request) {
-        return ResponseEntity.ok(new DtoResponse<>(1L, List.of(documentsHeaderService.add(request))));
-    }
+  @PostMapping()
+  public ResponseEntity<DtoResponse<DocumentHeaderAndBodyDto>> add(
+      @RequestBody DocumentHeaderRequest request) {
+    return ResponseEntity.ok(new DtoResponse<>(1L, List.of(documentsHeaderService.add(request))));
+  }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteById(@PathVariable long id) {
-        documentsHeaderService.deleteById(id);
-        return ResponseEntity.ok("Запись удалена");
-    }
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> deleteById(@PathVariable long id) {
+    documentsHeaderService.deleteById(id);
+    return ResponseEntity.ok("Запись удалена");
+  }
 
-    @GetMapping("/deleting")
-    public ResponseEntity<DtoResponse<DocumentHeaderDto>> getDeleted(
-            @RequestParam(name = "offset", defaultValue = "0") int offset,
-            @RequestParam(name = "limit", defaultValue = "10") int limit) {
-        RequestParametersForDocHeader parameter =
-                RequestParameterUtil.getObjectOfRequestParametersOfDocumentHeader(offset, limit, "", null,
-                        null, null, null, null, new String[]{"date"}, "ASC");
-        return ResponseEntity.ok(documentsHeaderService.getDeleted(parameter));
-    }
+  @GetMapping("/deleting")
+  public ResponseEntity<DtoResponse<DocumentHeaderDto>> getDeleted(
+      @RequestParam(name = "offset", defaultValue = "0") int offset,
+      @RequestParam(name = "limit", defaultValue = "10") int limit) {
+    RequestParametersForDocHeader parameter =
+        RequestParameterUtil.getObjectOfRequestParametersOfDocumentHeader(
+            offset, limit, "", null, null, null, null, null, new String[] {"date"}, "ASC");
+    return ResponseEntity.ok(documentsHeaderService.getDeleted(parameter));
+  }
 }
